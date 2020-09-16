c2103e8735e7d8442831ede952c4454668ac0ed6
Влад Концевич <kontsevichv@mail.ru>
2020-09-16 13:32:18 +0300

    Add Page Host For Page Animate Out

--- AIS Airoport/Pages/MainMenuPage.xaml.cs
+++ AIS Airoport/Pages/MainMenuPage.xaml.cs
@@ -3,13 +3,29 @@
 namespace AIS_Airoport
 {
     /// <summary>
-    /// Логика взаимодействия для MainMenuPage.xaml
+    /// Interaction logic for LoginPage.xaml
     /// </summary>
     public partial class MainMenuPage : BasePage<MainMenuViewModel>
     {
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public MainMenuPage()
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Constructor with specific view model
+        /// </summary>
+        /// <param name="specificViewModel">The specific view model to use for this page</param>
+        public MainMenuPage(MainMenuViewModel specificViewModel = null) : base(specificViewModel)
+        {
+            InitializeComponent();
+        }
+
+        #endregion
     }
 }
---
+++ AIS Airoport/ValueConverters/ApplicationPageHelpers.cs
@@ -0,0 +1,73 @@
+﻿using AIS_Airoport.Core;
+using System;
+using System.Diagnostics;
+
+namespace AIS_Airoport
+{
+    /// <summary>
+    /// Converts the <see cref="ApplicationPage"/> to an actual view/page
+    /// </summary>
+    public static class ApplicationPageHelpers
+    {
+        /// <summary>
+        /// Takes a <see cref="ApplicationPage"/> and a view model, if any, and creates the dessired page
+        /// </summary>
+        /// <param name="page"></param>
+        /// <param name="viewModel"></param>
+        /// <returns></returns>
+        public static BasePage ToBasePage(this ApplicationPage page, object viewModel = null)
+        {
+            // Find the appropriate page
+            switch (page)
+            {
+                case ApplicationPage.Login:
+                    return new LoginPage(viewModel as LoginViewModel);
+
+                case ApplicationPage.Register:
+                    throw new NotImplementedException("Implement Register Page");
+
+                case ApplicationPage.MainMenu:
+                    return new MainMenuPage(viewModel as MainMenuViewModel);
+
+                case ApplicationPage.TicketSelling:
+                    throw new NotImplementedException("Implement TicketSelling Page");
+
+                case ApplicationPage.FlightList:
+                    throw new NotImplementedException("Implement FlightList Page");
+
+                case ApplicationPage.Passengers:
+                    throw new NotImplementedException("Implement Passengers Page");
+
+                case ApplicationPage.Statistics:
+                    throw new NotImplementedException("Implement Statistics Page");
+
+                default:
+                    Debugger.Break();
+                    return null;
+            }
+        }
+
+        /// <summary>
+        /// Converts a <see cref="BasePage"/> to the specific <see cref="ApplicationPage"/> that is for that type of page
+        /// </summary>
+        /// <param name="page"></param>
+        /// <param name="viewModel"></param>
+        /// <returns></returns>
+        public static ApplicationPage ToApplicationPage(this BasePage page)
+        {
+            // Find application page that matches the base page
+            if (page is MainMenuPage)
+                return ApplicationPage.MainMenu;
+
+            if (page is LoginPage)
+                return ApplicationPage.Login;
+
+            //if (page is RegisterPage)
+            //    return ApplicationPage.Register;
+
+            // Alert developer of issue
+            Debugger.Break();
+            return default;
+        }
+    }
+}
