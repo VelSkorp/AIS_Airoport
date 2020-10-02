adc680939b6e74d7c0752538610b457e0adbab58
Влад Концевич <kontsevichv@mail.ru>
2020-10-02 12:11:30 +0300

    Fixed the transition of the passenger page to the main menu and spaces are replaced by tabs

--- AIS Airoport.Core/ViewModel/PassengersList/PassengersListViewModel.cs
+++ AIS Airoport.Core/ViewModel/PassengersList/PassengersListViewModel.cs
@@ -3,111 +3,111 @@ using System.Windows.Input;
 
 namespace AIS_Airoport.Core
 {
-    /// <summary>
-    /// The View Model for a passengers list screen
-    /// </summary>
-    public class PassengersListViewModel : BaseViewModel
-    {
-        #region Protected Members
+	/// <summary>
+	/// The View Model for a passengers list screen
+	/// </summary>
+	public class PassengersListViewModel : BaseViewModel
+	{
+		#region Protected Members
 
-        /// <summary>
-        /// The flight list items for the list
-        /// </summary>
-        protected ObservableCollection<PassengerViewModel> mItems;
+		/// <summary>
+		/// The flight list items for the list
+		/// </summary>
+		protected ObservableCollection<PassengerViewModel> mItems;
 
-        #endregion
+		#endregion
 
-        #region Public Properties
+		#region Public Properties
 
-        /// <summary>
-        /// The flight list items for the list
-        /// NOTE: Do not call Items.Add to add messages to this list
-        ///		  as it will make the FilteredIAndSortedtems out of sync
-        /// </summary>
-        public ObservableCollection<PassengerViewModel> Items
-        {
-            get => mItems;
-            set
-            {
-                // Make sure list has changed
-                if (mItems == value)
-                    return;
+		/// <summary>
+		/// The flight list items for the list
+		/// NOTE: Do not call Items.Add to add messages to this list
+		///		  as it will make the FilteredIAndSortedtems out of sync
+		/// </summary>
+		public ObservableCollection<PassengerViewModel> Items
+		{
+			get => mItems;
+			set
+			{
+				// Make sure list has changed
+				if (mItems == value)
+					return;
 
-                // Update value
-                mItems = value;
+				// Update value
+				mItems = value;
 
-                // Update filtered list to match
-                FilteredIAndSortedtems = new ObservableCollection<PassengerViewModel>(mItems);
-            }
-        }
+				// Update filtered list to match
+				FilteredIAndSortedtems = new ObservableCollection<PassengerViewModel>(mItems);
+			}
+		}
 
-        /// <summary>
-        /// The flight list items for the list that include search filtering
-        /// </summary>
-        public ObservableCollection<PassengerViewModel> FilteredIAndSortedtems { get; set; }
+		/// <summary>
+		/// The flight list items for the list that include search filtering
+		/// </summary>
+		public ObservableCollection<PassengerViewModel> FilteredIAndSortedtems { get; set; }
 
-        #endregion
+		#endregion
 
-        #region Commands
+		#region Commands
 
-        /// <summary>
-        /// The command to add a new passenger
-        /// </summary>
-        public ICommand AddCommand { get; set; }
+		/// <summary>
+		/// The command to add a new passenger
+		/// </summary>
+		public ICommand AddCommand { get; set; }
 
-        /// <summary>
-        /// The command to change data about the current passenger
-        /// </summary>
-        public ICommand ChangeCommand { get; set; }
+		/// <summary>
+		/// The command to change data about the current passenger
+		/// </summary>
+		public ICommand ChangeCommand { get; set; }
 
-        /// <summary>
-        /// The command to go back to the main menu
-        /// </summary>
-        public ICommand BackCommand { get; set; }
+		/// <summary>
+		/// The command to go back to the main menu
+		/// </summary>
+		public ICommand BackCommand { get; set; }
 
-        #endregion
+		#endregion
 
-        #region Constructor
+		#region Constructor
 
-        /// <summary>
-        /// Default constructor
-        /// </summary>
-        public PassengersListViewModel()
-        {
-            // Create commands
-            AddCommand = new RelayCommand(AddNewPassenger);
-            ChangeCommand = new RelayCommand(ChangeCurrentPassenger);
-            BackCommand = new RelayCommand(Back);
-        }
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public PassengersListViewModel()
+		{
+			// Create commands
+			AddCommand = new RelayCommand(AddNewPassenger);
+			ChangeCommand = new RelayCommand(ChangeCurrentPassenger);
+			BackCommand = new RelayCommand(Back);
+		}
 
-        #endregion
+		#endregion
 
-        #region Command Methods
+		#region Command Methods
 
-        /// <summary>
-        /// Go back to main menu page
-        /// </summary>
-        public void Back()
-        {
-            IoC.Application.GoToPage(ApplicationPage.MainMenu);
-        }
+		/// <summary>
+		/// Go back to main menu page
+		/// </summary>
+		public void Back()
+		{
+			IoC.Application.GoToPage(ApplicationPage.MainMenu);
+		}
 
-        /// <summary>
-        /// Adds information about the new passenger
-        /// </summary>
-        public void AddNewPassenger()
-        {
-            // TODO: Implement method to add information about the new passenger
-        }
+		/// <summary>
+		/// Adds information about the new passenger
+		/// </summary>
+		public void AddNewPassenger()
+		{
+			// TODO: Implement method to add information about the new passenger
+		}
 
-        /// <summary>
-        /// Changes data about the current passenger
-        /// </summary>
-        public void ChangeCurrentPassenger()
-        {
-            // TODO: Implement method to change data about the current passenger
-        }
+		/// <summary>
+		/// Changes data about the current passenger
+		/// </summary>
+		public void ChangeCurrentPassenger()
+		{
+			// TODO: Implement method to change data about the current passenger
+		}
 
-        #endregion
-    }
+		#endregion
+	}
 }
--- AIS Airoport/ValueConverters/ApplicationPageHelpers.cs
+++ AIS Airoport/ValueConverters/ApplicationPageHelpers.cs
@@ -4,76 +4,79 @@ using System.Diagnostics;
 
 namespace AIS_Airoport
 {
-    /// <summary>
-    /// Converts the <see cref="ApplicationPage"/> to an actual view/page
-    /// </summary>
-    public static class ApplicationPageHelpers
-    {
-        /// <summary>
-        /// Takes a <see cref="ApplicationPage"/> and a view model, if any, and creates the dessired page
-        /// </summary>
-        /// <param name="page"></param>
-        /// <param name="viewModel"></param>
-        /// <returns></returns>
-        public static BasePage ToBasePage(this ApplicationPage page, object viewModel = null)
-        {
-            // Find the appropriate page
-            switch (page)
-            {
-                case ApplicationPage.Login:
-                    return new LoginPage(viewModel as LoginViewModel);
+	/// <summary>
+	/// Converts the <see cref="ApplicationPage"/> to an actual view/page
+	/// </summary>
+	public static class ApplicationPageHelpers
+	{
+		/// <summary>
+		/// Takes a <see cref="ApplicationPage"/> and a view model, if any, and creates the dessired page
+		/// </summary>
+		/// <param name="page"></param>
+		/// <param name="viewModel"></param>
+		/// <returns></returns>
+		public static BasePage ToBasePage(this ApplicationPage page, object viewModel = null)
+		{
+			// Find the appropriate page
+			switch (page)
+			{
+				case ApplicationPage.Login:
+					return new LoginPage(viewModel as LoginViewModel);
 
-                case ApplicationPage.Register:
-                    throw new NotImplementedException("Implement Register Page");
+				case ApplicationPage.Register:
+					throw new NotImplementedException("Implement Register Page");
 
-                case ApplicationPage.MainMenu:
-                    return new MainMenuPage(viewModel as MainMenuViewModel);
+				case ApplicationPage.MainMenu:
+					return new MainMenuPage(viewModel as MainMenuViewModel);
 
-                case ApplicationPage.TicketSelling:
-                    return new TicketSellingPage(viewModel as TicketSellingViewModel);
+				case ApplicationPage.TicketSelling:
+					return new TicketSellingPage(viewModel as TicketSellingViewModel);
 
-                case ApplicationPage.FlightList:
-                    return new FlightListPage(viewModel as FlightListViewModel);
+				case ApplicationPage.FlightList:
+					return new FlightListPage(viewModel as FlightListViewModel);
 
-                case ApplicationPage.Passengers:
-                    return new PassengersPage(viewModel as PassengersListViewModel);
+				case ApplicationPage.Passengers:
+					return new PassengersPage(viewModel as PassengersListViewModel);
 
-                case ApplicationPage.Statistics:
-                    throw new NotImplementedException("Implement Statistics Page");
+				case ApplicationPage.Statistics:
+					throw new NotImplementedException("Implement Statistics Page");
 
-                default:
-                    Debugger.Break();
-                    return null;
-            }
-        }
+				default:
+					Debugger.Break();
+					return null;
+			}
+		}
 
-        /// <summary>
-        /// Converts a <see cref="BasePage"/> to the specific <see cref="ApplicationPage"/> that is for that type of page
-        /// </summary>
-        /// <param name="page"></param>
-        /// <param name="viewModel"></param>
-        /// <returns></returns>
-        public static ApplicationPage ToApplicationPage(this BasePage page)
-        {
-            // Find application page that matches the base page
-            if (page is MainMenuPage)
-                return ApplicationPage.MainMenu;
+		/// <summary>
+		/// Converts a <see cref="BasePage"/> to the specific <see cref="ApplicationPage"/> that is for that type of page
+		/// </summary>
+		/// <param name="page"></param>
+		/// <param name="viewModel"></param>
+		/// <returns></returns>
+		public static ApplicationPage ToApplicationPage(this BasePage page)
+		{
+			// Find application page that matches the base page
+			if (page is MainMenuPage)
+				return ApplicationPage.MainMenu;
 
-            if (page is FlightListPage)
-                return ApplicationPage.FlightList;
+			if (page is FlightListPage)
+				return ApplicationPage.FlightList;
 
-            if (page is TicketSellingPage)
-                return ApplicationPage.TicketSelling;
+			if (page is PassengersPage)
+				return ApplicationPage.Passengers;
 
-            if (page is LoginPage)
-                return ApplicationPage.Login;
+			if (page is TicketSellingPage)
+				return ApplicationPage.TicketSelling;
 
-            //if (page is RegisterPage)
-            //    return ApplicationPage.Register;
+			if (page is LoginPage)
+				return ApplicationPage.Login;
 
-            // Alert developer of issue
-            Debugger.Break();
-            return default;
-        }
-    }
+			//if (page is RegisterPage)
+			//    return ApplicationPage.Register;
+
+			// Alert developer of issue
+			Debugger.Break();
+			return default;
+		}
+	}
 }
