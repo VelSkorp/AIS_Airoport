b91a81dac434d40304a0f242e34594dd6af6113d
Влад Концевич <kontsevichv@mail.ru>
2020-10-05 06:44:33 +0300

    Development of the statistics page started

---
+++ AIS Airoport.Core/ViewModel/Statistics/StatisticsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AIS_Airoport.Core
+{
+    /// <summary>
+    /// The View Model for a ticket statistics screen
+    /// </summary>
+    public class StatisticsViewModel : BaseViewModel
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public StatisticsViewModel()
+        {
+            // Create commands
+
+        }
+
+        #endregion
+    }
+}
---
+++ AIS Airoport/Pages/StatisticsPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using AIS_Airoport.Core;
+
+namespace AIS_Airoport
+{
+    /// <summary>
+    /// Interaction logic for StatisticsPage.xaml
+    /// </summary>
+    public partial class StatisticsPage : BasePage<StatisticsViewModel>
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public StatisticsPage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Constructor with specific view model
+        /// </summary>
+        /// <param name="specificViewModel">The specific view model to use for this page</param>
+        public StatisticsPage(StatisticsViewModel specificViewModel = null) : base(specificViewModel)
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
