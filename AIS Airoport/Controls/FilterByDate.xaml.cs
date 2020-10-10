00e06dacb8e4629c5aa4c36aec4cfea575bb11a1
Влад Концевич <kontsevichv@mail.ru>
2020-10-10 11:59:03 +0300

    Date filter moved to control

---
+++ AIS Airoport/Controls/FilterByDate.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace AIS_Airoport
+{
+    /// <summary>
+    /// Логика взаимодействия для FilterByDate.xaml
+    /// </summary>
+    public partial class FilterByDate : UserControl
+    {
+        public FilterByDate()
+        {
+            InitializeComponent();
+        }
+    }
+}
