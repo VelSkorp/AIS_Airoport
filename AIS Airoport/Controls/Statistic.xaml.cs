c4d33bab9158f938691cc1d4cf758cc6831e4103
Влад Концевич <kontsevichv@mail.ru>
2020-10-10 09:42:17 +0300

    Static selection in a control

---
+++ AIS Airoport/Controls/Statistic.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace AIS_Airoport
+{
+    /// <summary>
+    /// Логика взаимодействия для Statistic.xaml
+    /// </summary>
+    public partial class Statistic : UserControl
+    {
+        public Statistic()
+        {
+            InitializeComponent();
+        }
+    }
+}
